c9b7611a57b623149cccfbe33f40ebe265b20bd8
Saikrishna <saikrishna580@gmail.com>
2026-07-31 16:46:34 +0530

    oil and karam added

--- 40-databases/parameters.tf
+++ 40-databases/parameters.tf
@@ -1,5 +1,4 @@
 resource "aws_ssm_parameter" "sg_id" {
-  count = length(var.sg_names)
   name  = "/${var.project}/${var.environment}/mysql_root_password" 
   type  = "SecureString"
   value = var.mysql_root_password
