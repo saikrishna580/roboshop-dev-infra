80d4147d9eb795df6830c81d254ab5725206de97
Saikrishna <saikrishna580@gmail.com>
2026-08-05 19:48:16 +0530

    terraform

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -55,3 +55,46 @@ resource "aws_ami_from_instance" "catalogue" {
     local.common_tags
   )
 }
+
+resource "aws_launch_template" "catalogue" {
+  name = "${local.common_name}-catalogue"
+
+
+  image_id = aws_ami_from_instance.catalogue.id # AMI ID
+
+  instance_initiated_shutdown_behavior = "terminate"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = ["local.catalogue_sg_id"]
+  update_default_version = true
+
+  # once the instances are created, these will become instance tags
+  tag_specifications {
+    resource_type = "instance"
+
+      tags = merge(
+      {
+          Name = "${local.common_name}-catalogue-${var.app_version}-${aws_instance.catalogue.id}"
+      },
+      local.common_tags
+    )
+  }
+
+  # once the instances are created, these will become volume tags
+  tag_specifications {
+    resource_type = "volume"
+
+      tags = merge(
+      {
+          Name = "${local.common_name}-catalogue-${var.app_version}-${aws_instance.catalogue.id}"
+      },
+      local.common_tags
+    )
+  }
+  # launch template resource tags
+  tags = merge(
+      {
+          Name = "${local.common_name}-catalogue-${var.app_version}-${aws_instance.catalogue.id}"
+      },
+      local.common_tags
+    )
+}
