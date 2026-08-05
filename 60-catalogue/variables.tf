19b02b8d13e015b0d0867432529de9a38622b16e
Saikrishna <saikrishna580@gmail.com>
2026-08-05 14:11:57 +0530

    Added backend-alb and catalogue

---
+++ 60-catalogue/variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+  default = "roboshop"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+# variable "zone_id" {
+#   default = "Z0092064164JTP81UU8C4"
+# }
+
+# variable "domain_name" {
+#    default = "daw84s.store"
+# }
