76dba78859669e6c3f6c56e9a21ab5f2cdc262da
Saikrishna <saikrishna580@gmail.com>
2026-08-05 17:01:34 +0530

    terraform

--- 60-catalogue/bootstrap.sh
+++ 60-catalogue/bootstrap.sh
@@ -2,6 +2,7 @@
 
 component=$1 #catalogue
 environment=$2 #dev
+app_version =$3
 dnf install ansible -y
 mkdir -p /var/log/roboshop/
 chown -R ec2-user:ec2-user /var/log/roboshop
@@ -12,4 +13,4 @@ cd /home/ec2-user
 git clone https://github.com/saikrishna580/roboshop-ansible-v3.git
 cd roboshop-ansible-v3
 git pull
-ansible-playbook -e component=$component -e env=$environment roboshop.yaml  
+ansible-playbook -e component=$component -e env=$environment -e app_Version =$app_Version roboshop.yaml  
--- 60-catalogue/variables.tf
+++ 60-catalogue/variables.tf
@@ -6,6 +6,10 @@ variable "environment" {
   default = "dev"
 }
 
+variable "app_version" {
+  default =  "v3"
+}
+
 # variable "zone_id" {
 #   default = "Z0092064164JTP81UU8C4"
 # }
