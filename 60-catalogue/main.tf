5105f0862e2aeb22df51189e5991155209edeb1a
Saikrishna <saikrishna580@gmail.com>
2026-08-06 22:46:55 +0530

    terraform

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -192,8 +192,6 @@ resource "terraform_data" "catalogue_delete" {
 
  # executes where terraform is running
   provisioner "local-exec" {
-    command = [
-      "aws ec2 terminate-instances --instance-ids ${aws_instance.catalogue.id}"
-    ]
+    command = "aws ec2 terminate-instances --instance-ids ${aws_instance.catalogue.id}"
   }
 }
