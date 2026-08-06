b3eeb3764c3af42d73525e6e9b6c5004681cfab9
Saikrishna <saikrishna580@gmail.com>
2026-08-06 18:05:31 +0530

    terraform

--- 60-catalogue/data.tf
+++ 60-catalogue/data.tf
@@ -12,6 +12,11 @@ data "aws_ssm_parameter" "vpc_id" {
 }
 
 
+data "aws_ssm_parameter" "backend_alb_listener_arn" {
+    name   = "/${var.project}/${var.environment}/backend_alb_listener_arn"
+}
+
+
 data "aws_ami" "joindevops" {
   most_recent      = true
   owners           = ["973714476881"]
--- 60-catalogue/variables.tf
+++ 60-catalogue/variables.tf
@@ -10,10 +10,10 @@ variable "app_version" {
   default =  "v3"
 }
 
-# variable "zone_id" {
+/* # variable "zone_id" {
 #   default = "Z0092064164JTP81UU8C4"
-# }
+# } */
 
-# variable "domain_name" {
-#    default = "daw84s.store"
-# }
+variable "domain_name" {
+   default = "daw84s.store"
+}
